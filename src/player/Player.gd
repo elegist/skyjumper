f9d080b1ba63b13e56d472a25ae220412181733a
Robin Patzak <robinpatzak@gmail.com>
2021-10-19 01:24:50 +0200

    Fix wall running and jumping

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -3,18 +3,22 @@ extends KinematicBody
 var velocity: Vector3 = Vector3.ZERO
 
 var is_dashing: bool = false
+var is_wall_running: bool = false
 
 export var gravity: float = -40.0
 
-export var max_speed: float = 16.0
-export var acceleration: float = 70.0
-export var ground_friction: float = 60.0
-export var air_resistance: float = 50.0
+export var max_speed: float = 20.0
+export var acceleration: float = 100.0
+export var friction: float = 120.0
 export var jump_height: float = 18.0
+export var max_gravity_drag: float = 20.0
 export var dash_factor: int = 40
 
 var max_jump_count: int = 2
 var jump_count: int = 0
+
+var dash_direction: Vector3 = Vector3.ZERO
+var wall_normal: Vector3 = Vector3.ZERO
 
 export var mouse_sensitivity: Vector2 = Vector2(0.0025, 0.0025)
 export var right_stick_sensitivity: Vector2 = Vector2(0.07, 0.035)
@@ -37,7 +41,6 @@ onready var camera_pivot = $CameraRoot/CameraPivot
 onready var camera = $CameraRoot/CameraPivot/CameraSpringArm/Camera
 onready var animation_tree = $Mesh/AnimationTree
 
-onready var wall_check_forward = $Mesh/WallChecks/Forward
 onready var wall_check_left = $Mesh/WallChecks/Left
 onready var wall_check_right = $Mesh/WallChecks/Right
 
@@ -74,25 +77,32 @@ func _physics_process(delta: float) -> void:
 	
 	apply_gravity(delta)
 	
-	apply_movement(move_direction, delta)
+	if !is_on_wall():
+		apply_movement(move_direction, delta)
 	
 	if is_on_floor():
 		jump_count = max_jump_count
+		is_dashing = false
+		is_wall_running = false
 	
-	if Input.is_action_just_pressed("jump") && jump_count > 0:
+	if Input.is_action_just_pressed("jump") && jump_count > 0 && !is_on_wall():
 		apply_jump()
-		jump_count -= 1
 	elif Input.is_action_just_released("jump") && velocity.y > jump_height / 2:
 		velocity.y = jump_height / 2
 	
 	if Input.is_action_just_pressed("dash") && !is_on_floor():
 		apply_dash(move_direction, delta)
+		dash_direction = move_direction
 	
-	if Input.is_action_pressed("dash"):
-		if is_on_wall():
-			apply_wallrun(move_direction)
+	if is_on_wall():
+		wall_normal = get_slide_collision(0).normal
+		if Input.is_action_pressed("dash"):
+			wall_check_left.enabled = true
+			wall_check_right.enabled = true
+			apply_wall_run(delta)
+			if Input.is_action_just_pressed("jump"):
+				apply_wall_jump()
 	else:
-		wall_check_forward.enabled = false
 		wall_check_left.enabled = false
 		wall_check_right.enabled = false
 	
@@ -114,8 +124,13 @@ func _update_snap_vector() -> void:
 	snap_vector = -get_floor_normal() if is_on_floor() else Vector3.DOWN
 
 func apply_gravity(delta: float) -> void:
-	velocity.y += gravity * delta
-	velocity.y = clamp(velocity.y, gravity, jump_height)
+	if is_wall_running:
+		var wall_running_factor = 8
+		velocity.y += gravity / wall_running_factor * delta
+		velocity.y = clamp(velocity.y, gravity / wall_running_factor, max_gravity_drag / wall_running_factor)
+	else:
+		velocity.y += gravity * delta
+		velocity.y = clamp(velocity.y, gravity, max_gravity_drag)
 
 func apply_movement(move_direction: Vector3, delta: float) -> void:
 	if move_direction != Vector3.ZERO:
@@ -123,45 +138,47 @@ func apply_movement(move_direction: Vector3, delta: float) -> void:
 		velocity.z = velocity.move_toward(move_direction * max_speed, acceleration * delta).z
 		mesh.rotation.y = lerp_angle(mesh.rotation.y, atan2(move_direction.x, move_direction.z), 10 * delta)
 	else:
-		if is_on_floor():
-			velocity = velocity.move_toward(Vector3.ZERO, ground_friction * delta)
-		else:
-			velocity.x = velocity.move_toward(Vector3.ZERO, air_resistance * delta).x
-			velocity.z = velocity.move_toward(Vector3.ZERO, air_resistance * delta).z
+		velocity.x = velocity.move_toward(Vector3.ZERO, friction * delta).x
+		velocity.z = velocity.move_toward(Vector3.ZERO, friction * delta).z
 
 func apply_jump() -> void:
 	snap_vector = Vector3.ZERO
+	jump_count -= 1
 	velocity.y = jump_height
 
 func apply_dash(move_direction: Vector3, delta: float) -> void:
 	is_dashing = true
 	velocity.x = velocity.move_toward(move_direction * max_speed * dash_factor, acceleration * dash_factor * delta).x
 	velocity.z = velocity.move_toward(move_direction * max_speed * dash_factor, acceleration * dash_factor * delta).z
+	velocity.y = velocity.move_toward(Vector3.ZERO, friction * dash_factor * delta).y
 	yield(get_tree().create_timer(0.25), "timeout")
-	velocity.x = velocity.move_toward(Vector3.ZERO, 50).x
-	velocity.z = velocity.move_toward(Vector3.ZERO, 50).z
+	velocity.x = velocity.move_toward(Vector3.ZERO, friction * dash_factor * delta).x
+	velocity.z = velocity.move_toward(Vector3.ZERO, friction * dash_factor * delta).z
 	is_dashing = false
 
-func apply_wallrun(move_direction: Vector3) -> void:
-	wall_check_forward.enabled = true
-	wall_check_left.enabled = true
-	wall_check_right.enabled = true
-	
-	if wall_check_forward.is_colliding() && (!wall_check_left.is_colliding() || wall_check_right.is_colliding()):
-		velocity.y = max_speed
-	if wall_check_left.is_colliding() || wall_check_right.is_colliding():
-		velocity.x = move_direction.x * max_speed
-		velocity.z = move_direction.z * max_speed
-		velocity.y = 0.0
+func apply_wall_run(delta: float) -> void:
+	is_wall_running = true
+	velocity.x = sign(dash_direction.x) * max_speed
+	velocity.z = sign(dash_direction.z) * max_speed
+
+func apply_wall_jump() -> void:
+	is_wall_running = false
+	velocity.y = jump_height / 1.1
+	velocity.x = wall_normal.x * 50
+	velocity.z = wall_normal.z * 50
+	if round(wall_normal.x) != 0:
+		dash_direction.x *= -1
+	elif round(wall_normal.z) != 0:
+		dash_direction.z *= -1
 
 func apply_animations() -> void:
-	if is_on_wall():
+	if is_on_wall() && Input.is_action_pressed("dash"):
 		animation_tree.set("parameters/location/current", 2)
-		animation_tree.set("parameters/TimeScale/scale", 2)
+		animation_tree.set("parameters/TimeScale/scale", 2.0)
 		if wall_check_left.is_colliding():
-			animation_tree.set("parameters/wall/current", 1)
-		elif wall_check_right.is_colliding():
 			animation_tree.set("parameters/wall/current", 0)
+		if wall_check_right.is_colliding():
+			animation_tree.set("parameters/wall/current", 1)
 	else:
 		if !is_on_floor():
 			animation_tree.set("parameters/location/current", 1)
